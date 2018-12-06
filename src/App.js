5ea2f28378c1a36421a033331a72c48825e60f67
Wong Cho Hing <venus.wong@oocl.com>
2018-12-06 14:27:27 +0800

    init Router pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import './App.css';
+import AboutPage from './route/AboutPage.js'
+import NotFoundPage from './route/NotFoundPage.js'
+import UserPage from './route/UserPage.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <nav>
+            <ul>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/userpage">UserPage</Link>
+              </li>
+            </ul>
+            <hr/>
+            <Switch>
+              <Route exact path="/" component={() => <h1>Hello World!</h1>}/>
+              <Route path="/about" component={AboutPage}/>
+              <Route path="/userpage" component={UserPage}/>
+              <Route component={NotFoundPage}/>
+            </Switch>
+            </nav>
+          </div>
+        </Router>
       </div>
     );
   }
